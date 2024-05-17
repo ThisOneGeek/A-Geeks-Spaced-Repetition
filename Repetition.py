48ee7c73beb619ff2202b3b7ab3ae07c79a82aed
ThisOneGeek <140281826+ThisOneGeek@users.noreply.github.com>
2024-05-17 19:23:08 -0400

    Initial commit

---
+++ Repetition.py
@@ -0,0 +1,135 @@
+import pandas as pd
+from pandas import DataFrame
+import datetime
+
+'''
+cardFronts = ["What is datatype is the Python package 'Pandas' known for?"]
+cardBacks = ["Dataframes"]
+repNumber = [0]
+repDate = [datetime.datetime.today().date()]
+repDataFrame = DataFrame({'Front of Card': cardFronts, 'Back of Card': cardBacks, 'Date of Next Rep': repDate, 'Repetition Number': repNumber})
+print(repDataFrame)
+
+repDataFrame.to_excel('Repetition.xlsx', sheet_name='sheet1', index=False)
+a = pd.read_excel('Repetition.xlsx', sheet_name='sheet1')
+print(a['Back of Card'].to_list())
+'''
+
+class repetitionClass():
+    repDataFrame = []
+    cardsFailedIndex = []
+    cardsToRedoIndex = []
+    cardsFullFinishedIndex = []
+    isReviewing = False
+    cardsLeft = 0
+    cardsSucceeded = 0
+    cardsFailed = 0
+    currentCard = 0
+
+    def startup(self):
+        self.repDataFrame = pd.read_excel('Repetition.xlsx', sheet_name='sheet1', converters={'Front of Card':str, 'Back of Card':str, 'Date of Next Rep':datetime.datetime.date, 'Repetition Number':int})
+        for i in range(0, len(self.repDataFrame)):
+            #print((self.repDataFrame["Date of Next Rep"][i] - datetime.datetime.today().date()).days)
+            if self.repDataFrame["Date of Next Rep"][i] == datetime.datetime.today().date() or (self.repDataFrame["Date of Next Rep"][i] - datetime.datetime.today().date()).days < 0:
+                self.cardsToRedoIndex.append(i)
+        self.cardsLeft = len(self.cardsToRedoIndex)
+        #self.cardsToRedoIndex = list(range(0, len(self.repDataFrame)))
+        #self.repDataFrame["Date of Next Rep"].to_pydatetime()
+
+    def cardRight(self, indexNum):
+        #if self.repDataFrame.loc[self.cardsToRedoIndex[indexNum], "Repetition Number"] < 8:
+            #print("Yo")
+        self.repDataFrame.loc[self.cardsToRedoIndex[indexNum], "Repetition Number"] += 1
+        self.repDataFrame.loc[self.cardsToRedoIndex[indexNum], "Date of Next Rep"] = datetime.datetime.today().date() + datetime.timedelta(days = 2**int(self.repDataFrame["Repetition Number"][self.cardsToRedoIndex[indexNum]]))
+
+        #if self.repDataFrame.loc[self.cardsToRedoIndex[indexNum], "Repetition Number"] >= 8:
+            #self.cardsFullFinishedIndex.append(self.cardsToRedoIndex[indexNum])
+
+        #print(self.isReviewing)
+        if self.currentCard < (len(self.cardsToRedoIndex) - 1) and self.cardsLeft > 0:# and not self.isReviewing:
+            self.cardsSucceeded += 1
+            self.cardsLeft -= 1
+            #self.currentCard += 1
+        #elif self.isReviewing and self.cardsToRedoIndex.index(self.currentCard) < (len(self.cardsToRedoIndex) - 1):
+            #print(self.cardsToRedoIndex.index(self.currentCard) + 1)
+            self.currentCard += 1
+        elif len(self.cardsToRedoIndex) == 0:
+            self.cardsSucceeded += 1
+            self.cardsLeft -= 1
+            print("You Won")
+        else:
+            self.cardsSucceeded += 1
+            self.cardsLeft -= 1
+            self.finishSet()
+    def cardWrong(self, indexNum):
+        self.repDataFrame.loc[self.cardsToRedoIndex[indexNum], "Date of Next Rep"] = datetime.datetime.today().date()
+        self.repDataFrame.loc[self.cardsToRedoIndex[indexNum], "Repetition Number"] = 0
+        self.cardsFailedIndex.append(self.cardsToRedoIndex[indexNum])
+
+        #print(self.currentCard)
+        #print(len(self.repDataFrame) - 1)
+        #print(self.cardsFailed)
+
+        #print(self.isReviewing)
+        if self.currentCard < (len(self.cardsToRedoIndex) - 1) and self.cardsLeft > 0:# and not self.isReviewing:
+            self.cardsFailed += 1
+            self.cardsLeft -= 1
+            #self.currentCard += 1
+        #elif self.isReviewing and self.cardsToRedoIndex.index(self.currentCard) < (len(self.cardsToRedoIndex)):
+            #print(self.cardsToRedoIndex.index(self.currentCard) + 1)
+            self.currentCard += 1
+        else:
+            self.cardsFailed += 1
+            self.cardsLeft -= 1
+            #print(self.cardsToRedoIndex.index(self.currentCard) < (len(self.cardsToRedoIndex) - 1))
+            self.finishSet()
+
+
+    def getCard(self, indexNum):
+        return self.repDataFrame["Front of Card"][self.cardsToRedoIndex[indexNum]], self.repDataFrame["Back of Card"][self.cardsToRedoIndex[indexNum]]
+
+    def getDeckSize(self):
+        return len(self.repDataFrame)
+
+    def addCard(self, cFront, cBack):
+        newDF = DataFrame({'Front of Card': [cFront], 'Back of Card': [cBack], 'Date of Next Rep': [datetime.datetime.today().date()], 'Repetition Number': [0]})
+        self.repDataFrame = pd.concat([self.repDataFrame, newDF], ignore_index=True)
+
+    def removeCard(self, indexNum):
+        self.repDataFrame.drop(self.repDataFrame.index[self.cardsToRedoIndex[indexNum]], inplace=True)
+
+    def finishSet(self):
+        self.cardsLeft = self.cardsFailed
+        self.cardsFailed = 0
+        #print(self.cardsToRedoIndex)
+        #print(self.cardsFailedIndex)
+        self.cardsToRedoIndex = self.cardsFailedIndex
+        self.cardsFailedIndex = []
+        #print(self.cardsToRedoIndex)
+        self.currentCard = 0
+        #if len(self.cardsToRedoIndex) > 0:
+            #self.isReviewing = True
+
+    def allDone(self):
+        self.repDataFrame = self.repDataFrame.sample(frac=1).reset_index(drop=True)
+        self.repDataFrame.to_excel('Repetition.xlsx', sheet_name='sheet1', index=False)
+
+
+
+
+if __name__ == '__main__':
+    reps = repetitionClass()
+    reps.startup()
+    print(reps.repDataFrame["Date of Next Rep"][0])
+    reps.cardRight(0)
+    print(reps.repDataFrame["Date of Next Rep"][0])
+    reps.allDone()
+    print(reps.getDeckSize())
+    print(reps.getCard(0))
+    #reps.addCard("Apple", "Banana", datetime.datetime.today().date())
+    reps.addCard("Canada", "Oh Canada", datetime.datetime.today().date())
+    print(reps.repDataFrame.iloc[0])
+    print(reps.repDataFrame.iloc[1])
+    reps.allDone()
+    #reps.removeCard(1)
+    #print(reps.repDataFrame)
---
+++ main.py
@@ -0,0 +1,115 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+from kivy.core.text import LabelBase
+import Repetition
+import os
+LabelBase.register(name='Pixel',
+                   fn_regular='DePixelHalbfett.ttf')
+
+class Flashcards(Widget):
+    cardFront = ObjectProperty(None)
+    cardBack = ObjectProperty(None)
+    cardsCompleted = ObjectProperty(None)
+    cardsMissed = ObjectProperty(None)
+    cardsRemaining = ObjectProperty(None)
+    repObj = Repetition.repetitionClass()
+    isShowFront = True
+    cardFS = 30
+
+    def startup(self):
+        self.repObj.startup()
+        self.cardFront.fs = self.cardFS
+        self.cardBack.fs = 0
+    def update(self):
+        #print(self.repObj.currentCard)
+        #print(self.repObj.repDataFrame)
+        if len(self.repObj.cardsToRedoIndex) > 0:
+            self.cardFront.text = self.repObj.repDataFrame["Front of Card"][self.repObj.cardsToRedoIndex[self.repObj.currentCard]]
+            self.cardBack.text = self.repObj.repDataFrame["Back of Card"][self.repObj.cardsToRedoIndex[self.repObj.currentCard]]
+        else:
+            self.cardFront.text = "All Done!"
+            self.cardBack.text = "All Done!!!!"
+            self.repObj.allDone()
+        self.cardsCompleted.text = str(self.repObj.cardsSucceeded)
+        self.cardsMissed.text = str(self.repObj.cardsFailed)
+        self.cardsRemaining.text = str(self.repObj.cardsLeft)
+        self.cardFront.fs = self.cardFS
+        self.cardBack.fs = 0
+        self.isShowFront = True
+
+
+    def dontKnowClicked(self):
+        if len(self.repObj.cardsToRedoIndex) > 0:
+            self.repObj.cardWrong(self.repObj.currentCard)
+            if self.repObj.cardsLeft == 0:
+                self.repObj.finishSet()
+        self.update()
+    def KnowClicked(self):
+        if len(self.repObj.cardsToRedoIndex) > 0:
+            self.repObj.cardRight(self.repObj.currentCard)
+            if self.repObj.cardsLeft == 0:
+                self.repObj.finishSet()
+        self.update()
+
+    def cardToggled(self):
+        #print(self.isShowFront)
+        if self.isShowFront:
+            self.cardFront.fs = 0
+            self.cardBack.fs = self.cardFS
+        else:
+            self.cardFront.fs = self.cardFS
+            self.cardBack.fs = 0
+        self.isShowFront = not self.isShowFront
+
+    def addCardButton(self):
+       self.show_popup()
+
+    def show_popup(self):
+        show = addCardPop(self.repObj)
+        pop = Popup(title="Add new Cards!", content=show, size_hint=(.6, .6), title_font="Pixel")
+        pop.bind(on_dismiss=self.popUpClosed)
+        pop.open()
+
+    def popUpClosed(self, instance):
+        self.repObj.startup()
+        self.update()
+
+
+class CardText(Widget):
+    text = StringProperty("")
+    fs = NumericProperty(0)
+
+class CardsLeft(Widget):
+    text = StringProperty("0")
+
+class addCardPop(FloatLayout):
+    #repObj = []
+    cardFrontInput = ObjectProperty(None)
+    def __init__(self, repObj):
+        super().__init__()
+        self.repObj = repObj
+    def addANewCard(self):
+        #print(len(self.ids.card_front_input.text))
+        #print(self.ids.card_back_input.text)
+        if len(self.ids.card_front_input.text) > 0 and len(self.ids.card_front_input.text) > 0:
+            self.repObj.addCard(self.ids.card_front_input.text, self.ids.card_back_input.text)
+        self.repObj.allDone()
+
+
+
+
+
+class SpacedRepetitionApp(App):
+    def build(self):
+        study = Flashcards()
+        study.startup()
+        study.update()
+        #print("hi")   Proof that it only plays everything up to the return once.
+        return study
+
+if __name__ == '__main__':
+    SpacedRepetitionApp().run()
+
